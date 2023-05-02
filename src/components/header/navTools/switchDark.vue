c1bfacedba835fdc9b920e68a4818de0f5e44bd2
Edwin Moreno <edwinmoreno328@gmail.com>
2023-05-02 08:07:16 -0500

    feature/transform to sfc

--- src/components/header/navTools/switchDark.vue
+++ src/components/header/navTools/switchDark.vue
@@ -1,24 +1,20 @@
-<script>
+<script setup lang="ts">
+import { storeToRefs } from 'pinia'
 import { useThemeSettingsStore } from '~/store/themeSettings'
-
 const themeSettingsStore = useThemeSettingsStore()
-
-export default {
-  methods: {
-    toogleDark() {
-      themeSettingsStore.toogleDark()
-    },
-  },
+const { isDark } = storeToRefs(themeSettingsStore)
+const toggleDark = () => {
+  themeSettingsStore.toggleDark()
 }
 </script>
 
 <template>
   <span
     class="lg:h-[32px] lg:w-[32px] lg:bg-gray-500-f7 bg-slate-50 lg:dark:bg-slate-900 dark:text-white text-slate-900 cursor-pointer rounded-full text-[20px] flex flex-col items-center justify-center"
-    @click="toogleDark"
+    @click="toggleDark"
   >
-    <Icon v-if="!$store.themeSettingsStore.isDark" icon="heroicons-outline:moon" />
-    <Icon v-else-if="$store.themeSettingsStore.isDark" icon="heroicons-outline:sun" />
+    <Icon v-if="!isDark" icon="heroicons-outline:moon" />
+    <Icon v-else icon="heroicons-outline:sun" />
   </span>
 </template>
 
