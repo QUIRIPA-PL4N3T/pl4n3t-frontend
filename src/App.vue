5a74f25aca2ed1c3ece84a1d930ca02f959370f6
Edwin Moreno <edwinmoreno328@gmail.com>
2023-11-14 10:04:45 -0500

    feature/add initial commit

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+// https://github.com/vueuse/head
+// you can use this to manipulate the document head in any components,
+// they will be rendered correctly in the html results with vite-ssg
+import { useThemeSettingsStore } from '~/stores/themeSettings'
+
+const themeSettingsStore = useThemeSettingsStore()
+provide('themeSettings', themeSettingsStore)
+
+useHead({
+  title: 'PL4N3T',
+  meta: [
+    { name: 'description', content: 'application to manage carbon footprint in companies' },
+    {
+      name: 'theme-color',
+      content: () => isDark.value ? '#00aba9' : '#ffffff',
+    },
+  ],
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/svg+xml',
+      href: () => preferredDark.value ? '/favicon-dark.svg' : '/favicon.svg',
+    },
+  ],
+})
+</script>
+
+<template>
+  <RouterView />
+</template>
---
+++ src/stores/themeSettings.ts
@@ -0,0 +1,64 @@
+import { defineStore } from 'pinia'
+
+export const useThemeSettingsStore = defineStore('themeSettings', {
+  state: () => ({
+    sidebarCollapse: false,
+    sidebarHidden: false,
+    mobileSidebar: false,
+    semiDark: false,
+    monochrome: false,
+    semiDarkTheme: 'semi-light',
+    isDark: false,
+    skin: 'default',
+    theme: 'light',
+    isOpenSettings: false,
+    cWidth: 'full',
+    menuLayout: 'vertical',
+    navbarType: 'sticky',
+    isMouseHovered: false,
+    footerType: 'static',
+    direction: false,
+    navbarColor: '',
+    chartColors: {
+      title: 'red',
+    },
+  }),
+  actions: {
+    setSidebarCollapse() {
+      this.sidebarCollapse = !this.sidebarCollapse
+    },
+
+    toggleDark() {
+      this.isDark = !this.isDark
+      document.body.classList.remove(this.theme)
+      this.theme = this.theme === 'dark' ? 'light' : 'dark'
+      document.body.classList.add(this.theme)
+      localStorage.setItem('theme', this.theme)
+    },
+
+    toggleMonochrome() {
+      const isMonochrome = (localStorage.getItem('monochrome') !== null)
+      // this.monochrome = !this.monochrome;
+      if (isMonochrome) {
+        localStorage.removeItem('monochrome')
+        document.getElementsByTagName('html')[0].classList.remove('grayscale')
+        return
+      }
+      localStorage.setItem('monochrome', 'true')
+      document.getElementsByTagName('html')[0].classList.add('grayscale')
+    },
+
+    toggleSettings() {
+      this.isOpenSettings = !this.isOpenSettings
+    },
+    toggleMobileSidebar() {
+      this.mobileSidebar = !this.mobileSidebar
+    },
+    toggleSemiDark() {
+      this.semiDark = !this.semiDark
+      this.semiDarkTheme = this.semiDark ? 'semi-dark' : 'semi-light'
+      document.body.classList.toggle(this.semiDarkTheme)
+      localStorage.setItem('semiDark', String(this.semiDark))
+    },
+  },
+})
