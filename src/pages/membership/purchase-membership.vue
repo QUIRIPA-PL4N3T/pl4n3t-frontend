6877828828c864be282b3b965584d47b8cc75993
jstcode99 <jstorres0211@gmail.com>
2024-05-16 17:12:05 -0500

    feat: install sdk mercado pago

--- src/pages/membership/purchase-membership.vue
+++ src/pages/membership/purchase-membership.vue
@@ -1,19 +1,65 @@
 <script setup lang="ts">
 import { storeToRefs } from 'pinia'
+import { useToast } from 'vue-toastification'
 import PaymentFields from '~/components/Payment/PaymentFields.vue'
+import { handleError } from '~/utilities/utils'
 
 const membershipsStore = useMembershipsStore()
+const companyStore = useCompanyStore()
 const router = useRouter()
 const { t } = useI18n()
+const toast = useToast()
+let errorMessage = $ref<string>('')
+let processing = $ref<boolean>(false)
+let catchPayment = $ref<boolean>(false)
+
+const mp = new MercadoPago(import.meta.env.VITE_MP_PUBLIC_KEY)
 
 membershipsStore.fetchMembership(Number(router.currentRoute.value.params.id))
-const { currentMembership } = storeToRefs(membershipsStore)
+const { company } = storeToRefs(companyStore)
+const { currentMembership, purchase } = storeToRefs(membershipsStore)
 
-function buyPlan(id: number) {
-  router.push({
-    name: 'purchase-status',
-    params: { id },
+function startMP(PREFERENCE_ID: string) {
+  mp.bricks().create('wallet', 'wallet_container', {
+    initialization: {
+      preferenceId: PREFERENCE_ID,
+    },
+    customization: {
+      texts: {
+        valueProp: currentMembership.value,
+      },
+    },
   })
+}
+
+async function purchasePost() {
+  processing = true
+  try {
+    membershipsStore.purchaseLoadBackUrl({
+      success: router.resolve({ name: 'purchase-status', params: { id: 1 } }).href,
+      failure: router.resolve({ name: 'purchase-status', params: { id: 1 } }).href,
+      pending: router.resolve({ name: 'purchase-status', params: { id: 1 } }).href,
+    })
+    await membershipsStore.purchaseMembership({
+      purchase: purchase.value,
+      companyId: company.value.id,
+      membershipId: currentMembership.value.id,
+    }).then(() => {
+      toast.success(t('purchase_membership.alerts.success'), {
+        timeout: 2000,
+      })
+      catchPayment = true
+      processing = false
+      startMP('sadasdsa')
+    }).catch(() => {
+      processing = false
+      toast.error(t('purchase_membership.alerts.error'))
+    })
+  }
+  catch (error: any) {
+    errorMessage = handleError(error)
+    toast.error(t('purchase_membership.alerts.error'))
+  }
 }
 </script>
 
@@ -36,12 +82,22 @@ function buyPlan(id: number) {
           </div>
           <div class="w-full">
             <FormKit
+              v-if="!catchPayment"
               type="form"
               :actions="false"
               :incomplete-message="false"
+              @submit="purchasePost"
             >
               <PaymentFields />
+              <button
+                type="submit" class="flex flex-wrap justify-center items-center gap-2 p-4 btn-dark dark:border-slate-400 w-full mt-4"
+              >
+                <Icon v-if="processing" icon="eos-icons:loading" class="p-0 m-0" />
+                {{ t('purchase_membership.button') }}
+              </button>
             </FormKit>
+            <div v-else id="wallet_container" class="w-full" />
+            <ElAlert v-if="errorMessage" class="mt-4" :title="errorMessage" type="error" show-icon />
           </div>
           <hr class="m-4">
           <div class="text-left mb-4">
@@ -51,12 +107,6 @@ function buyPlan(id: number) {
                 ${{ currentMembership.price !== '0.00' ? currentMembership.price : 'FREE' }}
               </span>
             </h2>
-            <Button
-              btn-class="btn-dark dark:border-slate-400 w-full mt-4"
-              @click="buyPlan(currentMembership.id)"
-            >
-              {{ t('purchase_membership.button') }}
-            </Button>
           </div>
         </div>
       </div>
